fd14f0b9006da74c1ad17a58778a951811f1159c
timofey1198 <timostar98@gmail.com>
2018-03-06 13:15:43 +0300

    Добавлен интерфейс IFromCSVReadable, позволяющий считывать данные из CSV файла в объект класса с данным интерфейсом

---
+++ kdz/IFromCSVReadable.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kdz.Model
+{
+    /// <summary>
+    /// Интерфейс, позволяющий заполнить поля 
+    /// класса при считывании CSV файла
+    /// </summary>
+    public interface IFromCSVReadable
+    {
+        /// <summary>
+        /// Функция, позволяющая заполнить поля
+        /// класса-наследника значениями из списка
+        /// </summary>
+        /// <typeparam name="T">Тип списка</typeparam>
+        /// <param name="values">Список значений для заполнения полей</param>
+        void SetFromList<T>(List<T> values);
+    }
+}
